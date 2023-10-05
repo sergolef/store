dc6cde87f12e9a18f14b9d566915ac1603130091
Serg Olef <sergolef@gmail.com>
2023-10-04 23:58:25 -0300

    section 2 lessons practises commit

---
+++ API/Extensions/ApplicationServicesExtension.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Infrastructure.Data;
+// using API.Helpers;
+// using API.Intrefaces;
+// using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServicesExtension
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            //services.Configure<CloudinaryConfig>(config.GetSection("CloudinarySettings"));
+
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 33));
+
+            //add repositories
+            // services.AddScoped<IUserRepository, UserRepository>();
+            // services.AddScoped<ILikeRepository, LikeRepository>();
+            // services.AddScoped<IMessageRepository, MessageRepository>();
+
+            services.AddDbContext<StoreContext>(options =>
+            {
+                options.UseMySql(connectionString, serverVersion);
+            });
+
+            // services.AddSpaStaticFiles(configuration =>
+            // {
+            //     configuration.RootPath = "ClientApp/dist";
+            // });
+
+            // //add services
+            // services.AddScoped<ITokenService, TokenService>();
+            // services.AddScoped<IPhotoService, PhotoService>();
+            // services.AddScoped<LogUserAcivity>();
+
+
+            // //add mapers
+            // services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
+
---
+++ API/Program.cs
@@ -0,0 +1,37 @@
+using API.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 33));
+
+builder.Services.AddAplicationServices(builder.Configuration); 
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
