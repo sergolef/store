46b130df57f9cbd8fb12f0ae1798f12e175526e1
Serg Olef <sergolef@gmail.com>
2023-12-04 01:09:33 -0400

    added fixes and added new service, order controller, implements unitOfWork pater for repositories

---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dtos;
+using API.Errors;
+using API.Extensions;
+using AutoMapper;
+using Core.Entities.OrderAggregate;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class OrdersController : BaseApiController
+    {
+        private readonly IOrderService _orderService;
+        private readonly IMapper _mapper;
+        public OrdersController(IOrderService orderService, IMapper mapper)
+        {
+            _orderService = orderService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Order>> CreateOrder(OrderOto orderDto){
+            var email = HttpContext.User.RetrieveEmailFromClaims();
+
+            var address = _mapper.Map<UserAddressDto, Address>(orderDto.ShipToAddress);
+
+            var order = await _orderService.CreateOrderAsync(email, orderDto.DeliveryMethodId, orderDto.BasketId, address);
+
+            if(order == null) return BadRequest(new ApiResponse(400, "Problem creating order"));
+
+            return Ok(order);
+
+        }
+
+        [HttpGet] 
+        public async Task<ActionResult<IReadOnlyList<OrderToReturnDto>>> GetOrdersForUser()
+        {
+            var email = HttpContext.User.RetrieveEmailFromClaims();
+
+            var orders = await _orderService.GetOrdersForUserAsync(email);
+
+            return Ok(_mapper.Map<IReadOnlyList<Order>, IReadOnlyList<OrderToReturnDto>>(orders));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderToReturnDto>> GetOrderForUserById(int id)
+        {
+            var email = HttpContext.User.RetrieveEmailFromClaims();
+
+            var order = await _orderService.GetOrderByIdAsync(id,email);
+
+            if(order == null) return BadRequest(new ApiResponse(404));
+
+            return Ok(_mapper.Map<Order, OrderToReturnDto>(order));
+
+        }
+
+        [HttpGet("deliveryMethods")]
+        public async Task<ActionResult<IReadOnlyList<DeliveryMethod>>> GetDeliveryMethods()
+        {
+            var methods = await _orderService.GetDeliveryMethodsAsync();
+            return Ok(methods);
+        }
+        
+    }
+}
